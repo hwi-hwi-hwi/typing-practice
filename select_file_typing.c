e12dec8b8a1c9c62602a0adb9c91b594174b0758
louis5103 <louis38625103@gmail.com>
2024-12-07 01:34:00 +0900

    Solution: Core dump
    Fragment Problem solved

--- select_file_typing.c
+++ select_file_typing.c
@@ -3,12 +3,7 @@
 #include <locale.h>
 #include <wchar.h>
 #include <stdlib.h>
-#include <time.h>    // 타이머 기능 위해 추가
-
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <time.h>
 
 #define BUFSIZE 256
 struct time_struct {
@@ -37,7 +32,7 @@ FILE *file, *save;
 
 int main(){
     setlocale(LC_ALL, "ko_KR.UTF-8");
-    
+
     struct buf_set buf = {{0}, };
     struct time_struct start_time, end_time;
 
@@ -45,7 +40,7 @@ int main(){
         perror("잘못된 파일 선택으로 종료되었습니다.");
         exit(-1);
     }
-        
+
     // 타이머 시작
     start_time = time_check();
 
@@ -66,12 +61,14 @@ int main(){
 
     // 통계 계산
     show_stat(total_correct_characters, total_typing_characters, incorrect_characters, (end_time.t - start_time.t));
+
     if(atexit(exit_process) ){
         perror("on_exit: no fclosed");
         exit(-1);
     }
     return 0;
 }
+
 FILE* choose_file(){
 // 파일 이름들 배열로 저장
     const char *files[] = {
@@ -79,7 +76,7 @@ FILE* choose_file(){
         "별_헤는_밤",
         "님의_침묵",
         "향수",
-        "test_txt"
+        "test"
     };
     int num_files = sizeof(files) / sizeof(files[0]);
 
@@ -104,8 +101,13 @@ FILE* choose_file(){
         return NULL;
     }
     file_name = files[choice - 1];
-    char *tmp_name; sprintf(tmp_name, "%s/%s.%s", "typing_sentences", file_name, "txt");
-    
+    char *tmp_name = (char *)malloc(sizeof(char) * 256);
+    if(tmp_name == NULL){
+        perror("Memory allocation failed");
+        exit(-1);
+    }
+    sprintf(tmp_name, "%s/%s.%s", "typing_sentences", file_name, "txt");
+
     FILE *tmp_file = fopen(tmp_name, "r");
     if (tmp_file == NULL) {
         perror("파일을 열 수 없습니다.");
@@ -120,7 +122,7 @@ struct time_struct time_check(){
     time_val.lt = localtime(&(time_val.t));
     strftime(time_val.buf, BUFSIZE, "%c\n", time_val.lt);
     return time_val;
-} 
+}
 
 void pre_processing(struct buf_set *buf, int *line_len, int *user_len){
     buf -> line[strcspn(buf->line, "\n")] = '\0';
@@ -142,7 +144,7 @@ void comp_string(int FGETS_LEN, int STDIN_LEN, wchar_t *FGETS_ARRAY, wchar_t *ST
             miss_count++;
         }
     }
-    
+
     int result_miss_count = ((miss_count >= FGETS_LEN) ? FGETS_LEN : miss_count);
 
     incorrect_characters += result_miss_count;
@@ -153,26 +155,29 @@ void comp_string(int FGETS_LEN, int STDIN_LEN, wchar_t *FGETS_ARRAY, wchar_t *ST
 
 void show_stat(int total_line, int typing_line, int total_miss, int total_time){
     save = fopen("record.txt", "r+t");
-    char line[BUFSIZE]; 
-    
+    char fline[BUFSIZE];
+
     double typing_per_min = (double) total_line * 60 / total_time;
     double typer_per_min = (double) typing_line * 60 / total_time;
-    double typing_speed = typing_per_min / 5;  
+    double typing_speed = typing_per_min / 5;
     double accuracy = (1 - ((double) total_miss / total_line))*100;
 
-    printf("======================================== RESULT ========================================\n\n");
-    printf("|  %-20s|  %-20s|  %-20s|  %-20s|  %-20s|  %-20s\n", "걸린 시간", "평균 타수(min)", "타이핑 속도", "정확도", "오타율", "선택한 파일"); 
+    printf("==================================================== RESULT ====================================================\n\n");
+    printf("|  %-20s|  %-20s|  %-20s|  %-20s|  %-20s|  %-20s\n", "걸린 시간", "평균 타수(min)", "타이핑 속도", "정확도", "오타율", "선택한 파일");
     printf("|  %-16d|  %-16.3f|  %-15.3f|  %-17.3f|  %-17.3f|  %-15s\n", total_time, typing_speed, typer_per_min, accuracy, 100-accuracy, file_name);
-    printf("\n======================================== RECORD ========================================\n\n");
+    printf("\n==================================================== RECORD ====================================================\n\n");
 
-    while(fgets(line, BUFSIZE, save) != NULL){
-        printf("%s\n", line);
+    while(fgets(fline, BUFSIZE, save) != NULL){
+        printf("%s\n", fline);
     }
     fprintf(save, "|  %-16d|  %-16.3f|  %-15.3f|  %-17.3f|  %-17.3f|  %-15s\n", total_time, typing_speed, typer_per_min, accuracy, 100-accuracy, file_name);
 }
 
 void exit_process(){
+    printf("[atexit]: fclose() start..\n");
     fclose(file);
     fclose(save);
+    printf("[atexit]: fclose() end..\n");
     exit(1);
+
 }
