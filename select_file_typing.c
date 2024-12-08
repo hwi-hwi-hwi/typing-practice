f763301212090109386d70cc655abe6cf47a0231
louis5103 <louis38625103@gmail.com>
2024-12-09 03:50:26 +0900

    최종본

--- select_file_typing.c
+++ select_file_typing.c
@@ -38,18 +38,21 @@ struct buf_set {
     wchar_t user_wstr[BUFSIZE];
 };
 
-void login(char **user_name, int flag);
-FILE* choose_file(char **file_name);
+void login(char**, int, char *, char *);
+FILE* choose_file(char**);
 void remove_record();
 void show_record();
-void pre_processing(struct buf_set* buf, int* line_len, int* user_len);
+void pre_processing(struct buf_set*, int*, int*);
 struct time_struct time_check();
-void comp_string(int FGETS_LEN, int STDIN_LEN, wchar_t* FGETS_ARRAY, wchar_t* STDIN_ARRAY, int* count, int *ic, int *tcc, int *ttc);
-void show_stat(FILE *save, char *file_name, char *username, int total_line, int typing_line, int total_miss, int total_time);
+void comp_string(int , int , wchar_t* , wchar_t* , int* , int *, int *, int *);
+void show_stat(FILE *, char *, char *, int , int , int , int );
+void copy_buf(char *, char *, int , int );
+void free_buf(char *, int , int );
+int comp_two(char *, char *);
 // void exit_process();
 
 jmp_buf jump_buf;
-char ID[BUFSIZE], PASSWORD[BUFSIZE];
+char ID[BUFSIZE], PASSWORD[BUFSIZ];
 
 int main() {
     setlocale(LC_ALL, "ko_KR.UTF-8");
@@ -64,27 +67,33 @@ int main() {
 
     if((flag=setjmp(jump_buf)) != 0){
         if(flag==100 || flag==101) { // 100: 일치하지 않는 ID, 101: 일치하지 않는 PW
-            *ID = 0;
-            *PASSWORD = 0;
             system("clear");
             printf("<------------- [ErrorCode: %d] ID 또는 PW가 일치하지 않아 처음으로 되돌아갑니다 ------------->\n", flag); 
             flag=0;
+            free_buf(ID, 0, BUFSIZE); free_buf(ID, 0, BUFSIZE);
+            // printf("초기화된 변수들: [ID:%s], [PASSWORD:%s], [Flag:%d]", ID, PASSWORD, flag);
+            
         }
         else if(flag==110) { // 110: 계정 생성 시, 중복 ID 존재.
-            // system("clear");
+            system("clear");
             printf("<------------- [ErrorCode: %d]계정 생성시에 중복되는 ID가 존재하여 처음으로 되돌아갑니다. ------------->\n", flag);
             flag=0;
+            free_buf(ID, 0, BUFSIZE); free_buf(ID, 0, BUFSIZE);
+            // printf("초기화된 변수들: [ID:%s], [PASSWORD:%s], [Flag:%d]", ID, PASSWORD, flag);
+            
         }
         else if(flag==200){ // 200: 계정 생성 시, ID 중복 확인하러 longjmp
+            // printf("200 flag 점프: [ID:%s], [PASSWORD:%s], [Flag:%d]", ID, PASSWORD, flag);
         }
         else if(flag==201){ // 201: 중복 검색 성공 후, 복귀 요청 시그널
-            // system("clear");
+            system("clear");
             printf("<------------- 중복되는 아이디가 없으므로 계정 생성 후 로그인합니다. ------------->\n!");
+            // printf("200 flag 점프: [ID:%s], [PASSWORD:%s], [Flag:%d]", ID, PASSWORD, flag);
             flag=202; // 계정 중복 검색 성공 시그널.
         }
         
     }
-    login(&user_name, flag);
+    login(&user_name, flag, ID, PASSWORD);
     if ((file = choose_file(&file_name)) == NULL) {
         perror("잘못된 파일 선택으로 종료되었습니다.");
         exit(-1);
@@ -105,15 +114,12 @@ int main() {
 
     end_time = time_check();
     printf("[ID:%s], [PW:%s], [user_name:%s]", ID, PASSWORD, user_name);
-    show_stat(save, file_name, user_name, total_correct_characters, total_typing_characters, incorrect_characters, (end_time.t - start_time.t));
+    show_stat(save, file_name, ID, total_correct_characters, total_typing_characters, incorrect_characters, (end_time.t - start_time.t));
     fclose(file);
-    // if(atexit(exit_process) ){
-    //     perror("on_exit: no fclosed");
-    //     exit(-1);
-    // }
+ 
     return 0;
 }
-void login(char **user_name, int flag){
+void login(char **user_name, int flag, char *ID, char *PASSWORD){
     int choice;
 
     if(flag==200) choice=1;
@@ -125,7 +131,7 @@ void login(char **user_name, int flag){
         printf("===============================================\n");
 
         scanf("%d", &choice);
-        while (getchar() != '\n');
+        // while (getchar() != '\n');
     }
 
     if (choice < 1 || choice > 2) {
@@ -135,49 +141,64 @@ void login(char **user_name, int flag){
     else if(choice==1){
         FILE *login_file = fopen("password.txt", "r");
         char buf[BUFSIZE];
-        char typing_id[BUFSIZE], typing_password[BUFSIZE];
+        free_buf(buf, 0, BUFSIZE);
+        // char typing_id[BUFSIZE], typing_password[BUFSIZE];
         int no_id=1;
 
         if(flag==0){
             printf("ID를 입력하시오. [한글과 , 기호는 사용불가.]\n");
-            scanf("%s", typing_id); while (getchar() != '\n');
+            scanf("%s", ID);  
             printf("PW를 입력하시오. [한글과 , 기호는 사용불가.]\n");
-            scanf("%s", typing_password); while (getchar() != '\n');
+            scanf("%s", PASSWORD);
+            printf("로그인 초기입력 [ID:%s], [PASSWORD:%s]\n", ID, PASSWORD); 
+        }
+        else if(flag==200){ // todo
+            
         }
         else{
-            strcpy(typing_id, user_name);
-            strcpy(typing_password, PASSWORD);
+            perror("Unkown flag 2");
+            exit(-1);
         }
-        // printf("user typing: [ID:%s], [PW:%s]\n", typing_id, typing_password);
 
         while(fgets(buf, BUFSIZE, login_file) != 0){
-            buf[strcspn(buf, "\n")]='\0';
+            char temp_id_cmp[BUFSIZE], temp_pw_cmp[BUFSIZE];
+            free_buf(temp_id_cmp, 0, BUFSIZE); free_buf(temp_pw_cmp, 0, BUFSIZE);
             
-            // printf("가져온 buf: [%s]\n", buf);
-            char *seperator = strchr(buf, ',');
-            char tmp_ID[seperator-buf];
-            // strncpy(tmp_ID, buf, sizeof(char)*(seperator-buf));
-            for(int i=0; i<(seperator-buf); i++){
-                tmp_ID[i] = buf[i];
+            char *seperator;
+            int i;
+            for(i=0; i<strlen(buf); i++){
+                if(buf[i]==',') {
+                    seperator = buf+i;
+                    printf("[%c] 발견\n", buf[i]);    
+                    break;
+                }
             }
-            // printf("1[buf: %s], [user_name: %s], [ID: %s], [PW: %s], [tmp_ID: %s], [typing_id: %s]\n", buf, user_name, ID, PASSWORD, tmp_ID, typing_id);
-            // printf("분리한 ID: %s\n", ID);
-            if(strcmp(tmp_ID, typing_id) == 0){
+            buf[strlen(buf)-1]==0;
+            
+            copy_buf(temp_id_cmp, buf, 0, seperator-buf);
+            copy_buf(temp_pw_cmp, buf, (seperator-buf)+1, strlen(buf)-1);
+            printf("파일에서 가져온 [ID:%s], [PASSWORD:%s]\n", temp_id_cmp, temp_pw_cmp);
+            
+            printf("ID에도 \\n이 포함되어있을까? %d==%d\n", strlen(ID), strlen(temp_id_cmp));
+            printf("PW에도 \\n이 포함되어있을까? %d==%d\n", strlen(PASSWORD), strlen(temp_pw_cmp));
+            if(comp_two(ID, temp_id_cmp)){
                 no_id = 0;
-                strcpy(ID, tmp_ID);
-                strncpy(PASSWORD, seperator+1, buf+(strlen(buf)-1)-seperator);
-                // printf("분리한 PASSWORD: %s\n", PASSWORD);
-                // printf("2[buf: %s], [user_name: %s], [ID: %s], [PW: %s], [tmp_ID: %s]\n\n", buf, user_name, ID, PASSWORD, tmp_ID);
+                
+
                 if(flag==200) longjmp(jump_buf, 110);
                 
-                if(strcmp(PASSWORD, typing_password) != 0){
+                
+                
+                if(comp_two(PASSWORD, temp_pw_cmp) == 0){
                     longjmp(jump_buf, 101);
                 }
-                // *user_name = ID;
-                strcpy(user_name, ID);
+                
+                system("clear");
                 printf("로그인 성공! [login: %s], [password: %s]\n", ID, PASSWORD);
                 break;
             }
+            free_buf(temp_id_cmp, 0, seperator-buf-1);
+            free_buf(temp_pw_cmp, (seperator-buf)+1, strlen(buf)-1);
         }
         if(no_id && flag==0){
             longjmp(jump_buf, 100);
@@ -193,20 +214,19 @@ void login(char **user_name, int flag){
         //TODO: login에 중복되는 ID가 있는지.
         if(flag==0){
             printf("생성할 ID를 입력하시오. [한글과 , 기호는 사용불가.]\n");
-            scanf("%s", ID);
+            scanf("%s", ID); 
             printf("생성할 PW를 입력하시오. [한글과 , 기호는 사용불가.]\n");
-            scanf("%s", PASSWORD);
-            strcpy(user_name, ID);
+            scanf("%s", PASSWORD); 
+            printf("계정 생성: 입력한 [ID:%s], [PASSWORD:%s]\n", ID, PASSWORD);
             longjmp(jump_buf, 200);
         }
         else if(flag==202) {
-            fprintf(login_file, "%s,%s\n", user_name, PASSWORD);
+            fprintf(login_file, "%s,%s\n", ID, PASSWORD);
         }
         else printf("Unknown Flag\n");
         
         fclose(login_file);
     }
-
 }
 FILE* choose_file(char **file_name){
     int num_files = sizeof(files) / sizeof(files[0]);
@@ -327,11 +347,30 @@ void show_stat(FILE *save, char *file_name, char *username, int total_line, int 
         total_time, typing_speed, typer_per_min, accuracy, 100 - accuracy, file_name, username);
     fclose(save);
 }
-// void exit_process(){
-//     printf("[atexit]: fclose() start..\n");
-//     fclose(file);
-//     fclose(save);
-//     printf("[atexit]: fclose() end..\n");
-//     exit(1);
-// }
-
+int comp_two(char *one, char *two){
+    
+    if(strlen(one) != strlen(two)) return 0;
+    for(int i=0; i<strlen(two); i++){
+        // printf("%c//%c\n", one[i], two[i]);
+        if (one[i]!=two[i]) return 0;
+    
+    }
+    return 1;
+}
+void copy_buf(char *dest, char *source, int start_index, int end_index){
+    int i;
+    for(i=0; i<(end_index-start_index); i++){
+        dest[i] = source[start_index+i];
+        if(dest[i]=='\n'){
+            printf("실제로 buf안 \\n도 받음.\n");
+        }
+    }
+    printf("copy_buf: [dest:%s]\n", dest);
+}
+void free_buf(char *dest, int start_index, int end_index){
+    int i;
+    for(i=0; i<(end_index-start_index); i++){
+        dest[i]=0;
+    }
+    printf("free_buf: [dest:%s]\n", dest);
+}
