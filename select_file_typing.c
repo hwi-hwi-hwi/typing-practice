64caebcc44025c4a12383415ea308aa6d62ff48a
louis5103 <louis38625103@gmail.com>
2024-12-08 16:18:59 +0900

    로그인 기능 추가 완료

--- select_file_typing.c
+++ select_file_typing.c
@@ -4,8 +4,9 @@
 #include <wchar.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <setjmp.h>
 #include <unistd.h>
+
 // #include <sys/types.h>
 // #include <sys/stat.h>
 #include <fcntl.h>
@@ -36,6 +37,7 @@ struct buf_set {
     wchar_t user_wstr[BUFSIZE];
 };
 
+void login(char **user_name, int flag);
 FILE* choose_file(char **file_name);
 void remove_record();
 void pre_processing(struct buf_set* buf, int* line_len, int* user_len);
@@ -44,17 +46,43 @@ void comp_string(int FGETS_LEN, int STDIN_LEN, wchar_t* FGETS_ARRAY, wchar_t* ST
 void show_stat(FILE *save, char *file_name, int total_line, int typing_line, int total_miss, int total_time);
 // void exit_process();
 
+jmp_buf jump_buf;
+char ID[BUFSIZE], PASSWORD[BUFSIZE];
+
 int main() {
     setlocale(LC_ALL, "ko_KR.UTF-8");
 
+    volatile int flag = 0;
     struct buf_set buf = {{0}, };
     struct time_struct start_time, end_time;
 
     int total_correct_characters=0, total_typing_characters=0, incorrect_characters=0;
-    char file_name[BUFSIZE], user_name[BUFSIZE];
-    FILE **file, **save;
+    char *file_name, *user_name;
+    FILE *file, *save;
 
-    // login();
+    if((flag=setjmp(jump_buf)) != 0){
+        if(flag==100 || flag==101) { // 100: 일치하지 않는 ID, 101: 일치하지 않는 PW
+            *ID = 0;
+            *PASSWORD = 0;
+            system("clear");
+            printf("<------------- [ErrorCode: %d] ID 또는 PW가 일치하지 않아 처음으로 되돌아갑니다 ------------->\n", flag); 
+            flag=0;
+        }
+        else if(flag==110) { // 110: 계정 생성 시, 중복 ID 존재.
+            system("clear");
+            printf("<------------- [ErrorCode: %d]계정 생성시에 중복되는 ID가 존재하여 처음으로 되돌아갑니다. ------------->\n", flag);
+            flag=0;
+        }
+        else if(flag==200){ // 200: 계정 생성 시, ID 중복 확인하러 longjmp
+        }
+        else if(flag==201){ // 201: 중복 검색 성공 후, 복귀 요청 시그널
+            system("clear");
+            printf("<------------- 중복되는 아이디가 없으므로 계정 생성 후 로그인합니다. ------------->\n!");
+            flag=202; // 계정 중복 검색 성공 시그널.
+        }
+        
+    }
+    login(&user_name, flag);
     if ((file = choose_file(&file_name)) == NULL) {
         perror("잘못된 파일 선택으로 종료되었습니다.");
         exit(-1);
@@ -82,7 +110,92 @@ int main() {
     // }
     return 0;
 }
+void login(char **user_name, int flag){
+    int choice;
 
+    if(flag==200) choice=1;
+    else if(flag==202) choice=2;// 200 to choice 1, 202 to choice 2
+    else{
+        printf("===============================================\n");
+        printf("1. 로그인\n");
+        printf("2. 계정 생성\n");
+        printf("===============================================\n");
+
+        scanf("%d", &choice);
+        while (getchar() != '\n');
+    }
+
+    if (choice < 1 || choice > 2) {
+        printf("잘못된 선택입니다.\n");
+        exit(-1);
+    }
+    else if(choice==1){
+        FILE *login_file = fopen("password.txt", "r");
+        char buf[BUFSIZE];
+        char typing_id[BUFSIZE], typing_password[BUFSIZE];
+        int no_id=1;
+
+        if(flag==0){
+            printf("ID를 입력하시오. [한글과 , 기호는 사용불가.]\n");
+            scanf("%s", typing_id); while (getchar() != '\n');
+            printf("PW를 입력하시오. [한글과 , 기호는 사용불가.]\n");
+            scanf("%s", typing_password); while (getchar() != '\n');
+        }
+        else{
+            strcpy(typing_id, ID);
+            strcpy(typing_password, PASSWORD);
+        }
+        // printf("user typing: [ID:%s], [PW:%s]\n", typing_id, typing_password);
+
+        while(fgets(buf, BUFSIZE, login_file) != 0){
+            buf[strcspn(buf, "\n")] = '\0';
+            // printf("가져온 buf: [%s]\n", buf);
+            char *seperator = strchr(buf, ',');
+
+            strncpy(ID, buf, seperator-buf);
+            // printf("분리한 ID: %s\n", ID);
+            if(strcmp(ID, typing_id) == 0){
+                no_id = 0;
+                strncpy(PASSWORD, seperator+1, buf+(strlen(buf)-1)-seperator);
+                // printf("분리한 PASSWORD: %s\n", PASSWORD);
+                if(flag==200) longjmp(jump_buf, 110);
+                
+                if(strcmp(PASSWORD, typing_password) != 0){
+                    longjmp(jump_buf, 101);
+                }
+                *user_name = ID;
+                printf("로그인 성공! [login: %s], [password: %s]\n", ID, PASSWORD);
+                break;
+            }
+        }
+        if(no_id && flag==0){
+            longjmp(jump_buf, 100);
+        }
+        else if(no_id && flag==200){
+            longjmp(jump_buf, 201); // 계정 생성 시 중복되는 아이디가 없음.
+        }
+        fclose(login_file);
+    }
+    else if(choice==2){
+        FILE *login_file = fopen("password.txt", "a+");
+
+        //TODO: login에 중복되는 ID가 있는지.
+        if(flag==0){
+            printf("생성할 ID를 입력하시오. [한글과 , 기호는 사용불가.]\n");
+            scanf("%s", ID);
+            printf("생성할 PW를 입력하시오. [한글과 , 기호는 사용불가.]\n");
+            scanf("%s", PASSWORD);
+            longjmp(jump_buf, 200);
+        }
+        else if(flag==202) {
+            fprintf(login_file, "%s,%s\n", ID, PASSWORD);
+        }
+        else printf("Unknown Flag\n");
+        
+        fclose(login_file);
+    }
+
+}
 FILE* choose_file(char **file_name){
     int num_files = sizeof(files) / sizeof(files[0]);
     const int num_utilities = sizeof(utilites) / sizeof(utilites[0]);
@@ -198,3 +311,8 @@ void show_stat(FILE *save, char *file_name, int total_line, int typing_line, int
 //     printf("[atexit]: fclose() end..\n");
 //     exit(1);
 // }
+
+
+// 로그인: ID 매칭 실패 - PW 매칭 실패 - 로그인 성공
+// 계정 생성: ID 중복 - 계정 생성 성공
+// 계정 생성 실패 후 로그인 선택 후 1 반복.
